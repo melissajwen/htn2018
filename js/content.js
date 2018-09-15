2d7381d13ceb5fac752ac5b26afe872fb731ed30
AndyJiang99 <andyjianguts@gmail.com>
2018-09-15 18:35:30 -0400

    pushing parody parrots

--- js/content.js
+++ js/content.js
@@ -1,17 +1,24 @@
-function record(){
-var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://accounts.spotify.com/api/token",
-    "method": "POST",
-    "headers": {
-      "content-type": "application/json"
-    },
-    "processData": false,
-    "data": "{\"grant_type\":\"client_credentials\",\"client_id\": \"c06e5e51b5404463817b6667c9ecfd13\",\"client_secret\": \"e4707d7cf5b84bfa993e1e0fdfe47296\",\"audience\": \"https://accounts.spotify.com/api\"}"
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
+images = new Array;
+images[0] = "/parrots/frame_01_delay-0.05s.jpg";
+images[1] = "/parrots/frame_02_delay-0.05s.jpg";
+images[2] = "/parrots/frame_03_delay-0.05s.jpg";
+images[3] = "/parrots/frame_04_delay-0.05s.jpg";
+images[4] = "/parrots/frame_05_delay-0.05s.jpg";
+images[5] = "/parrots/frame_06_delay-0.05s.jpg";
+images[6] = "/parrots/frame_07_delay-0.05s.jpg";
+images[7] = "/parrots/frame_08_delay-0.05s.jpg";
+images[8] = "/parrots/frame_09_delay-0.05s.jpg";
+images[9] = "/parrots/frame_00_delay-0.05s.jpg";
+setInterval(function() {changeImage()},250);
+x = 0;
+function changeImage() {
+document.getElementById('ad').src = images[x];
+if (x<8) {
+x+=1;
+}
+
+else if (x=9) {
+x=0;
+}
+
 }
