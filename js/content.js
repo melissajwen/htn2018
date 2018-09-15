e7a2608ad9f28d1b2fe54797645f7735813aeb2e
AndyJiang99 <andyjianguts@gmail.com>
2018-09-15 18:39:31 -0400

    complete parody parrot

--- js/content.js
+++ js/content.js
@@ -9,7 +9,7 @@ images[6] = "/parrots/frame_07_delay-0.05s.jpg";
 images[7] = "/parrots/frame_08_delay-0.05s.jpg";
 images[8] = "/parrots/frame_09_delay-0.05s.jpg";
 images[9] = "/parrots/frame_00_delay-0.05s.jpg";
-setInterval(function() {changeImage()},250);
+setInterval(function() {changeImage()},100);
 x = 0;
 function changeImage() {
 document.getElementById('ad').src = images[x];
